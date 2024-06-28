7907e63e97fc256bf1ac82d998680926b64776f9
Jul1enF <j.furic@gmail.com>
2024-06-28 14:08:27 +0200

    v2

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 fetch('https://whatyearisit-backend-iota-five.vercel.app/year')
 .then(response=>response.json())
 .then(data => {
-    document.querySelector('#year').textContent=data.year
+    document.querySelector('#year').textContent=`The current year is ${data.year} !`
 })
 
-console.log('')
